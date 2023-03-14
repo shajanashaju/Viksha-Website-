0577eef5b26fa4864db557cc8e32e240af059ef6
Amal a s <amalas@Amals-iMac-Pro.local>
2023-03-14 16:52:16 +0530

    almost complete

---
+++ js/slick.js
@@ -0,0 +1,14 @@
+
+$(document).ready(function(){
+    $(".slider-wrap").slick({
+
+        // normal options...
+        infinite: false,
+        centerMode:true,
+        nextArrow:false,
+        arrows:false,
+        adaptiveHeight:true,
+        autoplay:false,
+
+      });
+  });
