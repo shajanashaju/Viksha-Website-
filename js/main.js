50f0249cc14817c700a09c579da878bd74635208
shajanashaju <121115803+shajanashaju@users.noreply.github.com>
2023-03-23 10:48:56 +0530

    viksha-home

--- js/main.js
+++ js/main.js
@@ -1,17 +1,18 @@
 
-$(document).ready(function() {
+$(document).ready(function () {
   // alert("hi");
-    $(".mob-menu").click(function(){
-        $('.mob-menu-content').toggleClass("open-menu");
-      });
+  $(".mob-menu").click(function () {
+    $('.mob-menu-content').toggleClass("open-menu");
+  });
 
-      $(".mob-close").click(function(){
-        $('.mob-menu-content').toggleClass("open-menu");
-      });
-      $(".mob-close img").click(function(){
-        $('.mob-close img').toggleClass("rotate");
-      });
-      $(".mob-menu-content li").click(function(){
-        $('.mob-menu-content').toggleClass("open-menu");
-      });
+  $(".mob-close").click(function () {
+    $('.mob-menu-content').toggleClass("open-menu");
+  });
+  $(".mob-close img").click(function () {
+    $('.mob-close img').toggleClass("rotate");
+  });
+  $(".mob-menu-content li").click(function () {
+    $('.mob-menu-content').toggleClass("open-menu");
+  });
 });
+
